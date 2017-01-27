9494d768ffe2c0fe2670687bec9944f2842db0e3
Kevin <anticide@gmail.com>
2017-01-26 20:54:38 -0500

    codeigniter
    Added source and configured

---
+++ application/models/Account.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Account extends CI_Model {
+
+  public  function __construct() {
+
+  }
+
+  public function index() {
+    echo 'Index called';
+  }
+
+}
---
+++ application/views/home.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<html>
+<head>
+  <title><?php echo $title; ?></title>
+</head>
+<body>
+<header><h1>Coming Soon</h1></header>
+</body>
+</html>

3d24a1fdab1fa1fa9ba560c836454961f78a5642
Kevin <anticide@gmail.com>
2017-01-27 18:14:06 -0500

    Added badges

--- application/views/template/home.php
+++ application/views/template/home.php
@@ -1,12 +1,16 @@
 
 <div class="container maincontent">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-12">
+        <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-12">
             <div class="panel panel-primary">
                 <div class="panel-heading">
                     <h3 class="panel-title">Welcome to my web resume</h3></div>
                 <div class="panel-body">
-                  <p><span>I created this site to demonstrate my self-taught skills utilizing the following:</span></p>
+                  <p><span><h4>I created this site to demonstrate my self-taught skills utilizing the following:</h4></span></p>
+                  <img src="<?php echo base_url('assets/img/jquery.png');?>"/>
+                  <img src="<?php echo base_url('assets/img/codeigniter.png');?>"/>
+                  <img src="<?php echo base_url('assets/img/php.png');?>"/>
+                  <img src="<?php echo base_url('assets/img/mysql.png');?>"/>
 
                 </div>
             </div>

c86f0049e2206eb4e481931f0afe61e916644f85
Kevin <anticide@gmail.com>
2017-01-29 08:39:51 -0500

    Re-arranged view folders

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -35,7 +35,7 @@ public function index() {
 		$data['title']='KevinBisneau.com - Logged out';
 		$this->session->set_flashdata('msg', '<div class="alert alert-danger text-center">You are now logged out.</div>');
 		$this->load->view('template/header');
-		$this->load->view('login_view');
+		$this->load->view('pages/login_view');
 		$this->load->view('template/footer');
 
 	}
@@ -47,13 +47,13 @@ public function index() {
 		 $data['badges']=6;
 		 $data['badges']=$this->badges_model->badges($this->session->username);
 		 $this->load->view('template/header', $data);
-		 $this->load->view('template/resume', $data);
+		 $this->load->view('pages/resume', $data);
 		 $this->load->view('template/footer', $data);
 	 } else { //not logged in
 		 $data['title']='Not logged in';
 		 $this->session->set_flashdata('msg', '<div class="alert alert-danger text-center">You must be logged in to view this page.</div>');
 		 $this->load->view('template/header');
-		 $this->load->view('login_view');
+		 $this->load->view('pages/login_view');
 		 $this->load->view('template/footer');
 	 }
 	}
